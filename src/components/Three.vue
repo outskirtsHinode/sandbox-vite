95bf5e6e8f7a8e92373bb4c1a6d68af7ea717b15
tecchan <kishino@pepabo.com>
2022-12-02 01:31:41 +0900

    wip

--- src/components/Three.vue
+++ src/components/Three.vue
@@ -4,45 +4,131 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { GridHelper, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { AmbientLight, Color, DirectionalLight, Fog, Group, Mesh, MeshPhongMaterial, PerspectiveCamera, Scene, SphereGeometry, Vector3, WebGLRenderer } from "three";
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// 描画するDOMの指定
 const container = ref();
+
+const SCENE_PARAM = {
+  fogColor: 0x000000,
+  fogNear: 1.0,
+  fogFar: 27.0
+};
+const CAMERA_PARAM = {
+  fovy: 60,
+  aspect: window.innerWidth / window.innerHeight,
+  near: 0.1,
+  far: 30.0,
+  x: 0.0,
+  y: 5.0,
+  z: 7.0,
+  lookAt: new Vector3(0.0, 0.0, 0.0),
+};
+const RENDERER_PARAM = {
+  clearColor: 0x000000,
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const MATERIAL_PARAM = {
+  color: 0xfdfdf0,
+  specular: 0xffffff,
+};
+const DIRECTIONAL_LIGHT_PARAM = {
+  color: 0xda9a88,
+  intensity: 1.0,
+  x: 1.0,
+  y: 1.0,
+  z: 1.0
+};
+const AMBIENT_LIGHT_PARAM = {
+  color: 0xffffff,
+  intensity: 0.2,
+};
+
+// scene
 const scene = new Scene();
-const camera = new PerspectiveCamera();
+scene.fog = new Fog(
+  SCENE_PARAM.fogColor,
+  SCENE_PARAM.fogNear,
+  SCENE_PARAM.fogFar
+);
+const camera = new PerspectiveCamera(
+  CAMERA_PARAM.fovy,
+  CAMERA_PARAM.aspect,
+  CAMERA_PARAM.near,
+  CAMERA_PARAM.far
+);
 const renderer = new WebGLRenderer();
-// 初期化
+const composer = new EffectComposer(renderer);
+
 const init = () => {
   if (container.value instanceof HTMLElement) {
-    // DOMのサイズを取得
-    const { clientWidth, clientHeight } = container.value;
-    // 背景のグリッドの追加
-    scene.add(new GridHelper());
-    // カメラの設定
-    camera.aspect = clientWidth / clientHeight;
+    // camera
     camera.updateProjectionMatrix();
-    camera.position.set(10, 10, 0);
-    camera.lookAt(0, 0, 0);
-    // rendererの設定
-    renderer.setSize(clientWidth, clientHeight);
-    renderer.setPixelRatio(clientWidth / clientHeight);
+    camera.position.set(CAMERA_PARAM.x, CAMERA_PARAM.y, CAMERA_PARAM.z);
+    camera.lookAt(CAMERA_PARAM.lookAt);
+
+    // renderer
+    renderer.setClearColor(new Color(RENDERER_PARAM.clearColor));
+    renderer.setSize(RENDERER_PARAM.width, RENDERER_PARAM.height);
     container.value.appendChild(renderer.domElement);
-    // 描画
+
+    // composer
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+    const glitchPass = new GlitchPass();
+    composer.addPass(glitchPass);
+    glitchPass.renderToScreen = true;
+
+    // group
+    const group = new Group();
+
+    // material
+    const material = new MeshPhongMaterial(MATERIAL_PARAM);
+
+    // Geometry and Mesh
+    const sphereGeometry = new SphereGeometry(1.0, 16, 16);
+    const sphere = new Mesh(sphereGeometry, material);
+    group.add(sphere);
+    scene.add(group);
+
+    // directionalLight
+    const directionalLight = new DirectionalLight(
+      DIRECTIONAL_LIGHT_PARAM.color,
+      DIRECTIONAL_LIGHT_PARAM.intensity
+    );
+    directionalLight.position.x = DIRECTIONAL_LIGHT_PARAM.x;
+    directionalLight.position.y = DIRECTIONAL_LIGHT_PARAM.y;
+    directionalLight.position.z = DIRECTIONAL_LIGHT_PARAM.z;
+    scene.add(directionalLight);
+
+    // ambientLight
+    const ambientLight = new AmbientLight(
+      AMBIENT_LIGHT_PARAM.color,
+      AMBIENT_LIGHT_PARAM.intensity
+    );
+    scene.add(ambientLight);
+
+    // const axesHelper = new AxesHelper(5.0);
+    // scene.add(axesHelper);
+
+    // controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
     animate();
   }
 };
-// 描画
 const animate = () => {
   const frame = () => {
-    // 描画
-    renderer.render(scene, camera);
-    // 画面を更新
+    composer.render();
     requestAnimationFrame(frame);
   };
   frame();
 };
 
-// マウント時に初期化して描画
 onMounted(() => {
   init();
 });
@@ -51,8 +137,9 @@ onMounted(() => {
 <style scoped lang="scss">
 .three{
   position: relative;
-  width: 100vw;
-  height: 100vh;
-
+  top: 5vh;
+  left: 5vw;
+  width: 90vw;
+  height: 90vh;
 }
 </style>
